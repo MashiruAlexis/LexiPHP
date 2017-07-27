98f55cd463d51a9d52e777e13071a0763b1828e1
Ramon Alexis Celis <celisramon@ymail.com>
2017-07-27 19:09:04 +0800

    commit

--- app/code/client/arksystems/controller/index.php
+++ app/code/client/arksystems/controller/index.php
@@ -10,5 +10,6 @@ Class Arksystems_Controller_Index extends Frontend_Controller_Action {
 
 	public function setup() {
 		$this->setCss("arksystems/style");
+		$this->setJs("arksystems/script");
 	}
 }
