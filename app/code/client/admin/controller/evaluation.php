6ed803e7ee6fe2c73b1392274f9c16b901e7e0eb
Ramon Alexis Celis <celisramon@ymail.com>
2017-10-31 13:10:33 +0800

    updates

--- app/code/client/admin/controller/evaluation.php
+++ app/code/client/admin/controller/evaluation.php
@@ -7,11 +7,63 @@ Class Admin_Controller_Evaluation extends Frontend_Controller_Action {
 		$this->setBlock("admin/evaluation");
 	}
 
+	/**
+	 *	Add Criteria
+	 */
 	public function addCriteriaAction() {
 		$req = Core::getSingleton("url/request")->getRequest();
 		$evaluation = Core::getModel("admin/evaluation");
-		Core::log( $req );
-		Core::log( $evaluation );
+		$criteriaDb = Core::getModel("admin/criteria");
+		$alert = Core::getSingleton("system/session");
+
+		unset($req["btnAddCriteria"]);
+
+		if( empty($req["criteria"]) ) {
+			$alert->add("alert", [
+						"type" => "error",
+						"message" => "Some fields were empty."
+					]);
+			$this->_redirect("/admin/configure?tab=criteria");
+			return;
+		}
+
+		$criteriaDb->insert( ["label" => $req["criteria"]] );
+
+		if( isset($req["redirect"]) ) {
+			$this->_redirect($req["redirect"]);
+		}
+
+		$this->_redirect("/admin/configure?tab=criteria");
+	}
+
+	/**
+	 *	Add Sub Criteria
+	 */
+	public function addSubCriteriaAction() {
+		$req = Core::getSingleton("url/request")->getRequest();
+		$session = Core::getSingleton("system/session");
+		$subCriteriaDb = Core::getModel("admin/subcriteria");
+
+		if( isset($req["addSubCriteria"]) ) {
+			unset($req["addSubCriteria"]);
+		}
+
+		if( empty($req["subcriteria"]) or empty($req["criteria"]) ) {
+			$session->add("alert", [
+				"type" => "error",
+				"message" => "Please check all the fields before you submit."
+			]);
+			$this->_redirect("/admin/configure?tab=subcriteria");
+			return;
+		}
+
+		$subCriteriaDb->insert([
+			"evaluation_criteria_id" => $req["criteria"],
+			"question" => $req["subcriteria"]
+		]);
+
+		$this->_redirect("/admin/configure?tab=subcriteria");
+
 	}
 
 	public function setup() {
---
+++ app/code/client/admin/model/criteria.php
@@ -0,0 +1,7 @@
+<?php
+
+Class Admin_Model_Criteria extends Database_Model_Base {
+
+	protected $table = "evaluation_criteria";
+	
+}
