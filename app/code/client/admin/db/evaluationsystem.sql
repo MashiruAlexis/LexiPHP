53f41939fb248c5a64719655f7f02c374b33a0d9
Ramon Alexis Celis <celisramon@ymail.com>
2017-11-05 01:24:09 +0800

    updates

--- app/code/client/admin/db/evaluationsystem.sql
+++ app/code/client/admin/db/evaluationsystem.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.0.2
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2017 at 06:23 PM
--- Server version: 10.0.17-MariaDB
--- PHP Version: 5.6.14
+-- Generation Time: Oct 31, 2017 at 06:11 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,25 +30,13 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `account` (
   `id` int(11) NOT NULL,
-  `account_type_id` int(11) NOT NULL,
-  `fname` varchar(30) NOT NULL,
-  `lname` varchar(30) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `status` varchar(11) NOT NULL,
+  `status` int(11) NOT NULL,
   `updated_at` varchar(10) DEFAULT NULL,
   `created_at` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `account`
---
-
-INSERT INTO `account` (`id`, `account_type_id`, `fname`, `lname`, `username`, `password`, `email`, `status`, `updated_at`, `created_at`) VALUES
-(1, 1, 'Alexis', 'Celis', 'alexis', '$2y$10$C2S.05tIOwIv3', 'alexis@alexis.com', 'active', NULL, NULL),
-(2, 3, 'Alexis', 'Celis', 'alexis1', '$2y$10$u6wq/WpTbTZVw', 'alexis1@alexis.com', 'active', NULL, NULL),
-(3, 3, 'Alexis', 'Celis', 'alexis2', '$2y$10$ACntf5BBpvUbj', 'alexis2@alexis.com', 'active', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -69,17 +59,9 @@ CREATE TABLE `account_data` (
 
 CREATE TABLE `account_type` (
   `id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
   `type` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `account_type`
---
-
-INSERT INTO `account_type` (`id`, `type`) VALUES
-(1, 'Admin'),
-(2, 'Dean'),
-(3, 'Teacher');
 
 -- --------------------------------------------------------
 
@@ -89,21 +71,11 @@ INSERT INTO `account_type` (`id`, `type`) VALUES
 
 CREATE TABLE `evaluation` (
   `id` int(11) NOT NULL,
-  `evaluator_id` int(11) DEFAULT NULL,
+  `evaluator_id` int(11) NOT NULL,
   `account_id` int(11) NOT NULL,
-  `code` varchar(30) NOT NULL,
-  `sem` varchar(1040) DEFAULT NULL,
-  `school_year` int(11) DEFAULT NULL,
-  `status` varchar(20) NOT NULL
+  `sem` varchar(1040) NOT NULL,
+  `school_year` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `evaluation`
---
-
-INSERT INTO `evaluation` (`id`, `evaluator_id`, `account_id`, `code`, `sem`, `school_year`, `status`) VALUES
-(1, NULL, 3, 'COOLKID', NULL, NULL, 'on-going'),
-(2, NULL, 2, 'Wazzup', NULL, NULL, 'on-going');
 
 -- --------------------------------------------------------
 
@@ -122,10 +94,10 @@ CREATE TABLE `evaluation_criteria` (
 --
 
 INSERT INTO `evaluation_criteria` (`id`, `label`, `sort`) VALUES
-(1, 'This is criteria 1', NULL),
-(2, 'This is criteria 2', NULL),
-(3, 'This is criteria 3', NULL),
-(4, 'This is criteria 4', NULL);
+(1, 'A. Commitment', NULL),
+(2, 'B. Knowledge of Subject', NULL),
+(3, 'C. Teaching Independent Learning', NULL),
+(4, 'D. Management of Learning', NULL);
 
 -- --------------------------------------------------------
 
@@ -157,26 +129,10 @@ CREATE TABLE `evaluation_sub_criteria` (
 --
 
 INSERT INTO `evaluation_sub_criteria` (`id`, `evaluation_criteria_id`, `question`) VALUES
-(1, 1, 'This is sub criteria 1'),
-(2, 2, 'This is sub criteria 2'),
-(3, 1, 'This is sub criteria 2'),
-(4, 3, 'This is sub criteria 2'),
-(5, 4, 'This is sub criteria 1'),
-(6, 1, 'This is sub criteria 3'),
-(7, 1, 'This is sub criteria 4'),
-(8, 1, 'This is sub criteria 5'),
-(9, 2, 'This is sub criteria 1'),
-(10, 2, 'This is sub criteria 3'),
-(11, 2, 'This is sub criteria 4'),
-(12, 2, 'This is sub criteria 5'),
-(13, 3, 'This is sub criteria 1'),
-(14, 3, 'This is sub criteria 3'),
-(15, 3, 'This is sub criteria 3'),
-(16, 3, 'This is sub criteria 4'),
-(17, 4, 'This is sub criteria 2'),
-(18, 4, 'This is sub criteria 3'),
-(19, 4, 'This is sub criteria 4'),
-(20, 4, 'This is sub criteria 5');
+(1, 1, 'The student is learning properly.'),
+(2, 2, 'The student need more attention from the teacher.'),
+(3, 2, 'The students must have the availability to skip homework'),
+(4, 4, 'Students not responsible enough to learn on their own.');
 
 -- --------------------------------------------------------
 
@@ -213,7 +169,6 @@ CREATE TABLE `rating` (
 
 CREATE TABLE `subject` (
   `id` int(11) NOT NULL,
-  `account_id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -283,7 +238,7 @@ ALTER TABLE `subject`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `account_data`
 --
@@ -293,12 +248,12 @@ ALTER TABLE `account_data`
 -- AUTO_INCREMENT for table `account_type`
 --
 ALTER TABLE `account_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `evaluation`
 --
 ALTER TABLE `evaluation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `evaluation_criteria`
 --
@@ -313,7 +268,7 @@ ALTER TABLE `evaluation_data`
 -- AUTO_INCREMENT for table `evaluation_sub_criteria`
 --
 ALTER TABLE `evaluation_sub_criteria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `rating`
 --
@@ -323,7 +278,8 @@ ALTER TABLE `rating`
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
