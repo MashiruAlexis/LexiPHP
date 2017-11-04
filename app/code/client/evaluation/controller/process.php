2dca08e48def95f920828b7ad1ccb4de1dfa2be1
Ramon Alexis Celis <celisramon@ymail.com>
2017-11-05 02:51:37 +0800

    update

---
+++ app/code/client/evaluation/controller/process.php
@@ -0,0 +1,9 @@
+<?php
+
+Class Evaluation_Controller_Process extends Frontend_Controller_Action {
+
+	public function submitAction() {
+		$request = Core::getSingleton("url/request")->getRequest();
+		Core:log($request);
+	}
+}
