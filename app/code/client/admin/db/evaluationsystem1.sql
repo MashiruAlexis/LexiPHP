9a32350294337e75f13266005e8728a50aca113e
Ramon Alexis Celis <celisramon@ymail.com>
2017-11-09 23:51:09 +0800

    updates

--- app/code/client/admin/db/evaluationsystem1.sql
+++ app/code/client/admin/db/evaluationsystem1.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
+-- version 4.5.0.2
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2017 at 04:50 PM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.8
+-- Generation Time: Nov 04, 2017 at 06:23 PM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -34,7 +32,7 @@ CREATE TABLE `account` (
   `fname` varchar(30) NOT NULL,
   `lname` varchar(30) NOT NULL,
   `username` varchar(30) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
   `status` varchar(11) NOT NULL,
   `updated_at` varchar(10) DEFAULT NULL,
@@ -46,10 +44,9 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id`, `account_type_id`, `fname`, `lname`, `username`, `password`, `email`, `status`, `updated_at`, `created_at`) VALUES
-(7, 3, 'Alexis', 'Celis', 'alexis', '$2y$10$iMoF6w5soDPgGETESdaEwOi5alwQm36yUCkkyBUdhLj3tMjEkuSrm', 'alexis@alexis.com', 'active', NULL, NULL),
-(8, 3, 'Alexis', 'Celis', 'alexis1', '$2y$10$YAPZddTwIs2sNK0gz79zjOWNtri3RuCu4CLdsYWDlaYf6OHFddYvW', 'alexis1@alexis.com', 'active', NULL, NULL),
-(9, 3, 'Alexis', 'Celis', 'alexis2', '$2y$10$FEl/CsfTSjq1CelsjIPtyurxW4224LoN3sFULbfkjB9SNI/zQFTxS', 'alexis2@alexis.com', 'active', NULL, NULL),
-(10, 1, 'admin', 'admin', 'admin', '$2y$10$OmEACbSlmLsaWNhdJx3PReHUzV9WeRn0C9gfGIVAlAbu8zgD7Qg7i', 'admin@admin.com', 'active', NULL, NULL);
+(1, 1, 'Alexis', 'Celis', 'alexis', '$2y$10$C2S.05tIOwIv3', 'alexis@alexis.com', 'active', NULL, NULL),
+(2, 3, 'Alexis', 'Celis', 'alexis1', '$2y$10$u6wq/WpTbTZVw', 'alexis1@alexis.com', 'active', NULL, NULL),
+(3, 3, 'Alexis', 'Celis', 'alexis2', '$2y$10$ACntf5BBpvUbj', 'alexis2@alexis.com', 'active', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -105,7 +102,8 @@ CREATE TABLE `evaluation` (
 --
 
 INSERT INTO `evaluation` (`id`, `evaluator_id`, `account_id`, `code`, `sem`, `school_year`, `status`) VALUES
-(4, NULL, 7, 'WAZE', NULL, NULL, 'on-going');
+(1, NULL, 3, 'COOLKID', NULL, NULL, 'on-going'),
+(2, NULL, 2, 'Wazzup', NULL, NULL, 'on-going');
 
 -- --------------------------------------------------------
 
@@ -138,37 +136,9 @@ INSERT INTO `evaluation_criteria` (`id`, `label`, `sort`) VALUES
 CREATE TABLE `evaluation_data` (
   `id` int(11) NOT NULL,
   `evaluation_id` int(11) NOT NULL,
-  `evaluator_id` int(11) NOT NULL,
   `evaluation_sub_criteria_id` int(11) NOT NULL,
   `scale` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `evaluation_data`
---
-
-INSERT INTO `evaluation_data` (`id`, `evaluation_id`, `evaluator_id`, `evaluation_sub_criteria_id`, `scale`) VALUES
-(84, 4, 9, 1, '5'),
-(85, 4, 9, 3, '5'),
-(86, 4, 9, 6, '5'),
-(87, 4, 9, 7, '5'),
-(88, 4, 9, 8, '5'),
-(89, 4, 9, 2, '5'),
-(90, 4, 9, 9, '5'),
-(91, 4, 9, 10, '5'),
-(92, 4, 9, 11, '5'),
-(93, 4, 9, 12, '5'),
-(94, 4, 9, 4, '5'),
-(95, 4, 9, 13, '5'),
-(96, 4, 9, 14, '5'),
-(97, 4, 9, 15, '5'),
-(98, 4, 9, 16, '5'),
-(99, 4, 9, 5, '5'),
-(100, 4, 9, 17, '4'),
-(101, 4, 9, 18, '3'),
-(102, 4, 9, 19, '2'),
-(103, 4, 9, 20, '1'),
-(104, 4, 9, 0, 'Submit');
 
 -- --------------------------------------------------------
 
@@ -216,27 +186,11 @@ INSERT INTO `evaluation_sub_criteria` (`id`, `evaluation_criteria_id`, `question
 
 CREATE TABLE `evaluator` (
   `id` int(11) NOT NULL,
-  `account_id` int(11) DEFAULT NULL,
-  `type` varchar(20) DEFAULT NULL,
+  `type` varchar(20) NOT NULL,
   `name` varchar(30) NOT NULL,
-  `position` varchar(20) DEFAULT NULL,
-  `date` varchar(10) DEFAULT NULL
+  `position` varchar(20) NOT NULL,
+  `date` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `evaluator`
---
-
-INSERT INTO `evaluator` (`id`, `account_id`, `type`, `name`, `position`, `date`) VALUES
-(1, NULL, NULL, 'admin', 'admin', NULL),
-(2, NULL, NULL, 'admin', 'admin', NULL),
-(3, NULL, NULL, 'admin', 'admin', NULL),
-(4, NULL, NULL, 'admin', 'admin', NULL),
-(5, NULL, NULL, 'admin', 'admin', NULL),
-(6, NULL, NULL, 'admin', 'admin', NULL),
-(7, NULL, NULL, 'admin', 'admin', NULL),
-(8, NULL, NULL, 'admin', 'admin', NULL),
-(9, NULL, NULL, 'admin', 'admin', NULL);
 
 -- --------------------------------------------------------
 
@@ -310,12 +264,6 @@ ALTER TABLE `evaluation_sub_criteria`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `evaluator`
---
-ALTER TABLE `evaluator`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `rating`
 --
 ALTER TABLE `rating`
@@ -335,7 +283,7 @@ ALTER TABLE `subject`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `account_data`
 --
@@ -350,7 +298,7 @@ ALTER TABLE `account_type`
 -- AUTO_INCREMENT for table `evaluation`
 --
 ALTER TABLE `evaluation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `evaluation_criteria`
 --
@@ -360,17 +308,12 @@ ALTER TABLE `evaluation_criteria`
 -- AUTO_INCREMENT for table `evaluation_data`
 --
 ALTER TABLE `evaluation_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `evaluation_sub_criteria`
 --
 ALTER TABLE `evaluation_sub_criteria`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
---
--- AUTO_INCREMENT for table `evaluator`
---
-ALTER TABLE `evaluator`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `rating`
 --
@@ -380,8 +323,7 @@ ALTER TABLE `rating`
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
