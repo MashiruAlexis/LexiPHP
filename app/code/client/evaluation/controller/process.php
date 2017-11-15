82c1aff6e98c1934ea13e646db609e9edb7503b1
Ramon Alexis Celis <celisramon@ymail.com>
2017-11-16 00:56:39 +0800

    update formula

--- app/code/client/evaluation/controller/process.php
+++ app/code/client/evaluation/controller/process.php
@@ -47,35 +47,37 @@ Class Evaluation_Controller_Process extends Frontend_Controller_Action {
 			"crit_A3" => $request[6],
 			"crit_A4" => $request[7],
 			"crit_A5" => $request[8],
-			"ave_crit1" => $this->getAve([$request[1],$request[3],$request[6],$request[7],$request[8]]),
+			"ave_crit1" => $this->getAve([$request[1],$request[3],$request[6],$request[7],$request[8]], 1),
 
 			"crit_B1" => $request[2],
 			"crit_B2" => $request[9],
 			"crit_B3" => $request[10],
 			"crit_B4" => $request[11],
 			"crit_B5" => $request[12],
-			"ave_crit2" => $this->getAve([$request[2],$request[9],$request[10],$request[11],$request[12]]),
+			"ave_crit2" => $this->getAve([$request[2],$request[9],$request[10],$request[11],$request[12]], 1),
 
 			"crit_C1" => $request[4],
 			"crit_C2" => $request[13],
 			"crit_C3" => $request[14],
 			"crit_C4" => $request[15],
 			"crit_C5" => $request[16],
-			"ave_crit3" => $this->getAve([$request[4],$request[13],$request[14],$request[15],$request[16]]),
+			"ave_crit3" => $this->getAve([$request[4],$request[13],$request[14],$request[15],$request[16]], 2),
 
 			"crit_D1" => $request[5],
 			"crit_D2" => $request[17],
 			"crit_D3" => $request[18],
 			"crit_D4" => $request[19],
 			"crit_D5" => $request[20],
-			"ave_crit4" => $this->getAve([$request[5],$request[17],$request[18],$request[19],$request[20]]),
+			"ave_crit4" => $this->getAve([$request[5],$request[17],$request[18],$request[19],$request[20]], 2),
 
 			"ave_total" => $this->getAve([
-				$this->getAve([$request[1],$request[3],$request[6],$request[7],$request[8]]),
-				$this->getAve([$request[2],$request[9],$request[10],$request[11],$request[12]]),
-				$this->getAve([$request[4],$request[13],$request[14],$request[15],$request[16]]),
-				$this->getAve([$request[5],$request[17],$request[18],$request[19],$request[20]]),
-			])
+				$this->getAve([$request[1],$request[3],$request[6],$request[7],$request[8]], 1),
+				$this->getAve([$request[2],$request[9],$request[10],$request[11],$request[12]], 1),
+				$this->getAve([$request[4],$request[13],$request[14],$request[15],$request[16]], 2),
+				$this->getAve([$request[5],$request[17],$request[18],$request[19],$request[20]], 2),
+			],
+			3
+		)
 		]);
 
 		$evalAccountData = Core::getModel("account/accountdata")->where("account_id", $evaluationDb->where("code", $_SESSION["evaluation"]["code"])->first()->account_id)->first();
@@ -94,13 +96,31 @@ Class Evaluation_Controller_Process extends Frontend_Controller_Action {
 		$this->_redirect( $next );
 	}
 
-	public function getAve($ans = array()) {
-		$ave = 0;
-		foreach( $ans as $an ) {
-			$ave = $ave + $an;
+	public function getAve($ans = array(), $type = false) {
+		$sum = 0;
+		if( $type == 1 ) {
+			foreach( $ans as $a ) {
+				$sum = $sum + $a;
+			}
+
+			return ((($sum / 25) * 0.20) * 100);
 		}
 
-		return $ave / count($ans);
+		if( $type == 2 ) {
+			foreach( $ans as $a ) {
+				$sum = $sum + $a;
+			}
+
+			return ((($sum / 25) * 0.30) * 100);
+		}
+
+		if( $type = 3 ) {
+			foreach( $ans as $a ) {
+				$sum = ($sum + $a);
+			}
+			return $sum;
+		}
+		return 0;
 	}
 
 	public function evaluatorInfoAction () {
