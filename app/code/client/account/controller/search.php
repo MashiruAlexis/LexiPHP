d59057bd6d436e8533bc8279f52773d51c467d57
Ramon Alexis Celis <celisramon@ymail.com>
2018-02-25 09:17:15 +0800

    optimize search

--- app/code/client/account/controller/search.php
+++ app/code/client/account/controller/search.php
@@ -12,15 +12,32 @@ Class Account_Controller_Search extends Frontend_Controller_Action {
 	 *	@return json $res
 	 */
 	public function searchAction() {
-		$request = Core::getSingleton("url/request")->getRequest();
-		$accountDb = Core::getModel("account/account");
+		$request 		= Core::getSingleton("url/request")->getRequest();
+		$accountDb 		= Core::getModel("account/account");
+		$evaluationDb 	= Core::getModel("evaluation/evaluation");
 
 		$accountDb->like("fname", "%" . $request["name"] . "%" )->orLike( "lname", "%" . $request["name"] . "%" );
 		foreach( explode(" ", $request["name"] ) as $val ) {
 			$accountDb->orLike( "fname", "%" . $val . "%" );
 			$accountDb->orLike( "lname", "%" . $val . "%" );
 		}
-		echo json_encode($accountDb->get(["id", "fname", "lname"])); exit();
+
+		$res = $accountDb->get(["id", "fname", "lname"]);
+		$data1 = array();
+		$dataEval = [];
+		
+		foreach( $evaluationDb->where("status", $evaluationDb::STATUS_ON_GOING)->get(["account_id"]) as $data ) {
+			$dataEval[] = $data->account_id;
+		}
+
+		if( count($res) ) {
+			foreach( $res as $data ) {
+				if( in_array($data->id, $dataEval)) {
+					$data1[] = $data;
+				}
+			}
+		}
+		echo json_encode($data1); exit();
 		return;
 	}
 }
