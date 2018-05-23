15c61b052aef569521b0f3eefc98442b2c28ed68
Ramon Alexis Celis <celisramon@ymail.com>
2018-05-23 16:37:31 +0800

    test

---
+++ app/code/client/test/controller/jsv1.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © Ramon Alexis Celis All rights reserved.
+ * See license file for more info.
+ */
+
+Class Test_Controller_Jsv1 extends Frontend_Controller_Action {
+
+	/**
+	 *	Default controller action
+	 */
+	public function indexAction() {
+		$this->setPageTitle('Test');
+		// code here
+	}
+}
