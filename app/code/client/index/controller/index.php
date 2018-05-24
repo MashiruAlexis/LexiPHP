9cbd687289288c3aa39d6492173765163d7afb6d
Ramon Alexis Celis <celisramon@ymail.com>
2018-05-24 18:59:40 +0800

    news

---
+++ app/code/client/index/controller/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © Ramon Alexis Celis All rights reserved.
+ * See license file for more info.
+ */
+
+Class Index_Controller_Index extends Frontend_Controller_Action {
+
+	/**
+	 *	Default controller action
+	 */
+	public function indexAction() {
+		$this->setPageTitle('LexiPHP');
+		$this->setBlock("index/index");
+	}
+}
