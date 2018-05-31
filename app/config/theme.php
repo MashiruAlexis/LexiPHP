eb425bcdea14f487da2c2b2c42507e63c9539fa0
Ramon Alexis Celis <celisramon@ymail.com>
2018-06-01 06:55:19 +0800

    swtich to mdb

--- app/config/theme.php
+++ app/config/theme.php
@@ -18,7 +18,10 @@ return [
 	 * ----------------------------------
 	 */
 	"css" => [
-		"default/material-kit",
+		"mdb/bootstrap.min",
+		"mdb/material-dashboard",
+		"mdb/font-awesome",
+		"mdb/google-roboto-300-700",
 	],
 
 	/**
@@ -27,10 +30,48 @@ return [
 	 * ----------------------------------
 	 */
 	"js" => [
-		"default/jquery.min",
-		"default/popper.min",
-		"default/bootstrap-material-design",
-		"default/material-kit",
+		"mdb/jquery-3.1.1.min",
+		"mdb/jquery-ui.min.js",
+		"mdb/bootstrap.min.js",
+		"mdb/material.min.js",
+		"mdb/perfect-scrollbar.jquery.min.js"
 	]
 
 ];
+
+// <!-- Forms Validations Plugin -->
+// <script src="../assets/js/jquery.validate.min.js"></script>
+// <!--  Plugin for Date Time Picker and Full Calendar Plugin-->
+// <script src="../assets/js/moment.min.js"></script>
+// <!--  Charts Plugin -->
+// <script src="../assets/js/chartist.min.js"></script>
+// <!--  Plugin for the Wizard -->
+// <script src="../assets/js/jquery.bootstrap-wizard.js"></script>
+// <!--  Notifications Plugin    -->
+// <script src="../assets/js/bootstrap-notify.js"></script>
+// <!--   Sharrre Library    -->
+// <script src="../assets/js/jquery.sharrre.js"></script>
+// <!-- DateTimePicker Plugin -->
+// <script src="../assets/js/bootstrap-datetimepicker.js"></script>
+// <!-- Vector Map plugin -->
+// <script src="../assets/js/jquery-jvectormap.js"></script>
+// <!-- Sliders Plugin -->
+// <script src="../assets/js/nouislider.min.js"></script>
+// <!--  Google Maps Plugin    -->
+// <!--<script src="../assets/js/jquery.select-bootstrap.js"></script>-->
+// <!-- Select Plugin -->
+// <script src="../assets/js/jquery.select-bootstrap.js"></script>
+// <!--  DataTables.net Plugin    -->
+// <script src="../assets/js/jquery.datatables.js"></script>
+// <!-- Sweet Alert 2 plugin -->
+// <script src="../assets/js/sweetalert2.js"></script>
+// <!--	Plugin for Fileupload, full documentation here: http:
+// <script src="../assets/js/jasny-bootstrap.min.js"></script>
+// <!--  Full Calendar Plugin    -->
+// <script src="../assets/js/fullcalendar.min.js"></script>
+// <!-- TagsInput Plugin -->
+// <script src="../assets/js/jquery.tagsinput.js"></script>
+// <!-- Material Dashboard javascript methods -->
+// <script src="../assets/js/material-dashboard.js"></script>
+// <!-- Material Dashboard DEMO methods, don't include it in your project! -->
+// <script src="../assets/js/demo.js"></script>
