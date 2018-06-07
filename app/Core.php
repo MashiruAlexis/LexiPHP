a96bc954433ff5b0990f92b46a025af0981216f4
Ramon Alexis Celis <celisramon@ymail.com>
2018-06-07 12:00:36 +0800

    uptimize core

--- app/Core.php
+++ app/Core.php
@@ -1,42 +1,10 @@
 <?php
 /**
- *
- * MIT License
- *
- * Copyright (c) 2017 Ramon Alexis Celis
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Copyright © Ramon Alexis Celis All rights reserved.
+ * See license file for more info.
  */
-ob_start();
-spl_autoload_register(function($class) {
-	$class = strtolower(str_replace("_", DIRECTORY_SEPARATOR, $class));
-	$paths = Core::$paths;
-	
-	foreach($paths as $path) {
-		$mainpath = $path . $class . ".php";
-		if(file_exists($mainpath)) {
-			return require_once $mainpath;
-		}
-	}
-});
 
-define("BPcore", "app/code/base/");
+spl_autoload_register(["Core", "autload"]);
 define("DS", DIRECTORY_SEPARATOR);
 define("PS", PATH_SEPARATOR);
 define("BP", dirname(dirname(__FILE__)));
@@ -46,7 +14,6 @@ define("BS", "/");
 $paths = array();
 $paths[] = BP . DS . "app" . DS . "code" . DS . "client" . DS;
 $paths[] = BP . DS . "app" . DS . "code" . DS . "base" . DS;
-$paths[] = BP . DS . "lib" . DS . "lexi" . DS;
 
 $skinPath = array();
 $skinPath[] = BP . DS . "skin" . DS . "client" . DS;
@@ -144,7 +111,7 @@ Class Core {
  				->setType(404)
  				->exec();
  		}
-
+ 		ob_start();
  		$kernel->setController( Core::getSingleton($kernel->getApp() . "/" . $kernel->getController()) );
  		if(method_exists($kernel->getController(), $kernel->getMethod())) {
  			call_user_func([$kernel->getController(), "loadThemeResource"]);
@@ -211,13 +178,6 @@ Class Core {
  		$model = explode("/", $model);
  		$model = $model[0] . "_model_" . $model[1];
  		return new $model;
- 	}
-
- 	/**
- 	 *	Return the parameters
- 	 */
- 	public static function getParams() {
- 		return self::params;
  	}
 
  	/**
@@ -321,29 +281,18 @@ Class Core {
  	}
 
  	/**
- 	 *	Exceptions
+ 	 *	Core Autoloader
  	 */
- 	public static function exceptionHandler( $e ) {
- 		echo "----- EXCEPTION -----";
- 		echo $e->getMessage();
- 		return;
- 	}
-
- 	/**
- 	 *	Errors
- 	 */
- 	public static function errorHandler( $severity, $message, $filepath = null, $line = 0 ) {
- 		echo "----- ERROR -----";
- 		echo $message;
- 		return;
- 	}
-
- 	/**
- 	 *	Shutdown
- 	 */
- 	public static function shutdownHandler() {
- 		echo "----- SHUTDOWN -----";
- 		self::log( error_get_last() );
+ 	public static function autload( $class ) {
+ 		$class = strtolower(str_replace("_", DIRECTORY_SEPARATOR, $class));
+		$paths = Core::$paths;
+		
+		foreach($paths as $path) {
+			$mainpath = $path . $class . ".php";
+			if(file_exists($mainpath)) {
+				return require_once $mainpath;
+			}
+		}
  	}
 
  }
